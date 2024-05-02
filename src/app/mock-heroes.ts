dfa98db142e8644f30a71952fff63cb6e64fe892
Anonimato2024 <166399568+Anonimato2024@users.noreply.github.com>
2024-05-02 09:21:03 -0400

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './heroes';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Spider Man' },
+  { id: 13, name: 'Pantera Negra' },
+  { id: 14, name: 'Iron Man' },
+  { id: 15, name: 'Thor' },
+  { id: 16, name: 'Superman' },
+  { id: 17, name: 'Batman' },
+  { id: 18, name: 'Venom' },
+  { id: 19, name: 'Dead Pool' },
+  { id: 20, name: 'Mistic' }
+];
